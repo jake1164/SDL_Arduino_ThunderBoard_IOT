9335459e216f2a396c8dc7d3303566579b7c8377
Jason Jackson <jake1164@hotmail.com>
2018-08-25 11:10:14 -0400

    Updated credentials with details of how to ignore / add changes.

--- credentials.h
+++ credentials.h
@@ -1,5 +1,11 @@
 /*
+  If you clone this repository and want to keep your credentials out of github then
+  execute the following command to ignore any changes to this file and prevent being
+  promted to add this file when you update other files in github.
+  # git update-index --assume-unchanged credentials.h
 
+  To continue tracking changes:
+  # git update-index --no-assume-unchanged credentials.h
 */
 #ifndef __CREDENTIALS_H__
 #define __CREDENTIALS_H__
